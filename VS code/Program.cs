037362b685e43e760e28fd7abf8c41b7ad28996d
MustafaKerimYilmaz <m.kerimyil.61@gmail.com>
2025-05-11 19:36:55 +0300

    Update Program.cs
    I added a necessary and basic code sequence for Program.cs

--- VS code/Program.cs
+++ VS code/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Threading.Tasks;
+
+namespace PetSimulatorGame
+{
+    class Program
+    {
+        static async Task Main(string[] args) // I wrote "async" so that R can use "await"
+        { 
+             Game game = new Game();
+             await game.GameLoop();
+        }
+    }
+}
