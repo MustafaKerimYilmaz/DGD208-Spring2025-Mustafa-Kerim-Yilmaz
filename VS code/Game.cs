ba0bf97bdaa28111bb911bbadc06f38eb487675f
MustafaKerimYilmaz <m.kerimyil.61@gmail.com>
2025-05-20 01:19:46 +0300

    Add files via upload

--- VS code/Game.cs
+++ VS code/Game.cs
@@ -19,21 +19,46 @@ public class Game
     private bool _isRunning;
     private List<Pet> _pets = new List<Pet>();
     private readonly Menu<string> _mainMenu;
-    private readonly StudentInfo _studentInfo = new StudentInfo(
+
+    private readonly StudentInfo _studentInfo = new StudentInfo(   
     "Mustafa Kerim Yilmaz",
     "229911278"
     );
-}
-public Game()
-{
-    _mainMenu = new Menu<string>("Main Menu", new List<string>
-    {
-        "Adopt a new pet",
-        "View your pets",
-        "Use an item on a pet",
-        "Show creator info"
-        "Exit"
-    }, item => item);
-}
 
-public async Task GameLoop()
+
+    public Game()
+    {
+        _mainMenu = new Menu<string>("Main Menu", new List<string>
+        {
+            "Adopt a new pet",
+            "View your pets",
+            "Use an item on a pet",
+            "Show creator info",
+            "Exit"
+        }, item => item);
+    }
+
+    public async Task GameLoop()
+    {
+        Initialize();
+
+        _isRunning = true;
+        while (_isRunning)
+        {
+
+            string userChoice = _mainMenu.ShowAndGetSelection();
+
+
+            await ProcessUserChoice(userChoice);
+        }
+
+        Console.WriteLine("Thanks for playing!");
+    }
+
+    private void Initialize()
+    {
+        Console.WriteLine("Welcome to the Pet Simulator!");
+        Console.WriteLine("Stats decrease over time. Keep them above 0 to keep your pets alive!");
+        Console.WriteLine("Press any key to continue...");
+        Console.ReadKey();
+    }
